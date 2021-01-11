71f3174d1073509f0a2e3a871d64ee9ac2688a8e
MansanSouleimanAhmed <msa.github.cv@gmail.com>
2021-01-11 17:38:34 +0100

    html and css

---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+const gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	sourcemaps = require('gulp-sourcemaps'),
+	browserSync = require('browser-sync').create(),
+	autoprefixer = require('autoprefixer'),
+	cssnano = require('cssnano'),
+	postcss = require('gulp-postcss'),
+	cleanCSS = require('gulp-clean-css'),
+	concat = require('gulp-concat'),
+	imagemin = require('gulp-imagemin'),
+	changed = require('gulp-changed'),
+	uglify = require('gulp-uglify'),
+	lineec = require('gulp-line-ending-corrector'),
+	JS_SRC = 'src/assets/js/*.js',
+	JS_DEST = 'src/assets/production/js/',
+	STYLE_SRC = 'src/assets/scss/*.scss',
+	STYLE_DEST = 'src/assets/production/css';
+// The server function called later
+function serve(done) {
+	browserSync.init({
+		server: {
+			baseDir: './src',
+			index: '/index.html',
+		},
+		port: 3000,
+		// open: false,
+	});
+	done();
+}
+
+//The server function to reload
+function reload(done) {
+	browserSync.reload();
+	done();
+}
+
+function jsTask() {
+	return gulp
+		.src(JS_SRC)
+		.pipe(sourcemaps.init())
+		.pipe(gulp.dest(JS_DEST))
+
+		.pipe(browserSync.stream());
+}
+
+//compile sass
+function style() {
+	var processors = [autoprefixer, cssnano];
+	return gulp
+		.src(STYLE_SRC)
+		.pipe(sass().on('error', sass.logError))
+		.pipe(postcss(processors)) /*add*/ // PostCSS plugins
+		.pipe(cleanCSS())
+		.pipe(sourcemaps.write()) // write sourcemaps file in current directory
+		.pipe(gulp.dest(STYLE_DEST))
+		.pipe(browserSync.stream());
+}
+/* const mimImages =() =>{
+} */
+function watch() {
+	gulp.watch(STYLE_SRC, gulp.series(style, reload));
+	gulp.watch(JS_SRC, gulp.series(jsTask, reload));
+	gulp.watch('src/*.html', reload);
+}
+
+//Compiling & Moving stylesheets & Scripts
+var files = gulp.parallel(style, jsTask);
+//Building task
+var build = gulp.series(files, gulp.parallel(serve, watch));
+gulp.task(build);
+gulp.task('default', build);
